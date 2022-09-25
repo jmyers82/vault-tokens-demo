c30684a8f58cf7d90ececc3b48d823b771076e67
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-25 10:19:34 -0600

    update auth backend

--- vault/app_role.tf
+++ vault/app_role.tf
@@ -11,14 +11,14 @@ resource "vault_auth_backend" "app_role_backend_auth" {
 
 resource "vault_approle_auth_backend_role" "aws_secrets" {
   provider       = vault.cloud
-  backend        = vault_auth_backend.approle.path
+  backend        = vault_auth_backend.app_role_backend_auth.path
   role_name      = "aws-create-role"
   token_policies = ["default"] #"aws-create", "aws-update"]
 }
 
 resource "vault_approle_auth_backend_role" "azure_secrets" {
   provider       = vault.cloud
-  backend        = vault_auth_backend.approle.path
+  backend        = vault_auth_backend.app_role_backend_auth.path
   role_name      = "azure-create-role"
   token_policies = ["default"] #"azure-create", "azure-update"]
 }
