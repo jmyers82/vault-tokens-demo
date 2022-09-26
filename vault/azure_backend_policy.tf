0baea78f1b96893aa4e16afb36753fd34cef27cb
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-26 07:18:43 -0600

    update create policies

--- vault/azure_backend_policy.tf
+++ vault/azure_backend_policy.tf
@@ -1,4 +1,4 @@
-resource "vault_policy" "azure_read_policy" {
+resource "vault_policy" "azure_read_auth" {
   for_each = local.subscriptions
   provider = vault.cloud
   name     = "azure_read_creds_${each.value.sub_id}"
@@ -6,6 +6,20 @@ resource "vault_policy" "azure_read_policy" {
   policy = <<EOT
 path "azure/creds/${each.value.sub_id}" {
   capabilities = ["read"]
+}
+
+EOT
+}
+
+
+resource "vault_policy" "azure_create_auth" {
+  for_each = local.subscriptions
+  provider = vault.cloud
+  name     = "azure_create_creds_${each.value.sub_id}"
+
+  policy = <<EOT
+path "azure/creds/*" {
+  capabilities = ["create"]
 }
 
 path "auth/token/create" {
