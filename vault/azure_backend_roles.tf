4e217aacc7cb0201691c135df2dfda99c61e8c66
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-26 23:28:00 -0600

    updates

--- vault/azure_backend_roles.tf
+++ vault/azure_backend_roles.tf
@@ -1,3 +1,14 @@
+resource "vault_azure_secret_backend" "azure" {
+  provider                = vault.cloud
+  use_microsoft_graph_api = true
+  subscription_id         = "d995a747-3f1a-412a-bf34-655f5d44cc0f"
+  tenant_id               = "46eb567a-7cf3-48cc-9a90-f6deab9bb040"
+  client_id               = "df8d18ab-3c48-4697-9884-65ee95ed4ae6"
+  client_secret           = var.azure_client_secret
+  environment             = "AzurePublicCloud"
+  path                    = "azure"
+}
+
 resource "vault_azure_secret_backend_role" "azure_contributor_role" {
   for_each = local.subscriptions
   provider = vault.cloud
