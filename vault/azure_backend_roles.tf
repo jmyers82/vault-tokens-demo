5f83b0bf6936852756402911ed124ed054f4f847
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-26 23:22:15 -0600

    update role

--- vault/azure_backend_roles.tf
+++ vault/azure_backend_roles.tf
@@ -2,7 +2,8 @@ resource "vault_azure_secret_backend_role" "azure_contributor_role" {
   for_each = local.subscriptions
   provider = vault.cloud
   backend  = vault_azure_secret_backend.azure.path
-  role     = format("%s_contributor", each.value.sub_id)
+  role     = "${each.value.sub_id}_contributor"
+  # role     = format("%s_contributor", each.value.sub_id)
   ttl      = 14400
   max_ttl  = 28800
 
