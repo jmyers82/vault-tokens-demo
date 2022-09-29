460877607a93b39d1dab7dfa92adf94ccda5fc10
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-29 08:34:28 -0600

    add keyvault

---
+++ terraform/modules/keyvault.tf
@@ -0,0 +1,20 @@
+
+resource "azurerm_key_vault" "hashi_vault" {
+  name                        = "hashivault"
+  location                    = azurerm_resource_group.hashi_terraform_demo.location
+  resource_group_name         = azurerm_resource_group.hashi_terraform_demo.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+  enable_rbac_authorization = true
+
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "hashiconf-secret"
+  value        = "hashiconf-rocks!"
+  key_vault_id = azurerm_key_vault.hashi_vault.id
+}
