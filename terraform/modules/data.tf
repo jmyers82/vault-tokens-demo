357e1abfb722019f372ba94732e74f82310474c2
Jeremy <MyersJ@weyerhaeuser.com>
2022-09-29 11:37:04 -0600

    fix data

--- terraform/modules/data.tf
+++ terraform/modules/data.tf
@@ -2,10 +2,10 @@ data "azurerm_client_config" "current" {}
 
 data "vault_kv_secret_v2" "secret_hashiconf" {
   mount = "kvv2"
-  name  = hashiconf
+  name  = hashiconf/hashiconfsecret/Key
 }
 
 data "vault_kv_secret_v2" "secret_hashiswag" {
   mount = "kvv2"
-  name  = hashiswag
+  name  = hashiswag/whohas/Key
 }
